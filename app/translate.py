b40b0b24fdfbfb528645c7540174082a0ff77a23
Maksim Buturlakin <skvozik@gmail.com>
2020-05-21 18:25:04 +0600

    One more bug

--- app/translate.py
+++ app/translate.py
@@ -3,10 +3,10 @@ from flask import current_app
 
 
 def translate(text, source_language, dest_language):
-    if 'MS_TRANSLATOR_KEY' not in app.config or \
+    if 'MS_TRANSLATOR_KEY' not in current_app.config or \
             not current_app.config['MS_TRANSLATOR_KEY']:
         return 'Error: the translation service is not configured.'
-    auth = {'Ocp-Apim-Subscription-Key': app.config['MS_TRANSLATOR_KEY'],
+    auth = {'Ocp-Apim-Subscription-Key': current_app.config['MS_TRANSLATOR_KEY'],
             'Ocp-Apim-Subscription-Region': 'westeurope'}
 
     data = [
