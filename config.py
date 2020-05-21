c171a8846c50626229939665690e9603c5d785ec
Maksim Buturlakin <skvozik@gmail.com>
2020-05-21 19:12:42 +0600

    Heroku preparations

--- config.py
+++ config.py
@@ -21,3 +21,4 @@ class Config():
     LANGUAGES = ['en', 'ru']
     MS_TRANSLATOR_KEY = os.environ.get('MS_TRANSLATOR_KEY')
     ELASTICSEARCH_URL = os.environ.get('ELASTICSEARCH_URL')
+    LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
